0e22c545cce8bf63eca2533c0805c6d763b93ada
uyutaka <yutaka.urakami@colorado.edu>
2023-02-06 07:31:13 +0000

    Section 9 -> 76. Writing tests for our Render package I

---
+++ internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/uyutaka/bookings-go/internal/config"
+	"github.com/uyutaka/bookings-go/internal/models"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// Change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+	app = &testApp
+
+	os.Exit(m.Run())
+}
