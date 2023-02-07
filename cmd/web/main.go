4c4031d55e6fbd48a39f13724ef59c76424b5e82
uyutaka <yutaka.urakami@colorado.edu>
2023-02-07 05:09:26 +0000

    Section 9 -> 82. Centralizing our error handling to a helpers package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/uyutaka/bookings-go/internal/config"
 	"github.com/uyutaka/bookings-go/internal/handlers"
+	"github.com/uyutaka/bookings-go/internal/helpers"
 	"github.com/uyutaka/bookings-go/internal/models"
 	"github.com/uyutaka/bookings-go/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main function
 func main() {
@@ -46,6 +50,12 @@ func run() error {
 	// Change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+	
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -67,5 +77,7 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
 	return nil
 }
