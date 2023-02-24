7cba50479b9657ae8b8a40a8875a0a8b20265fe8
uyutaka <yutaka.urakami@colorado.edu>
2023-02-24 06:15:42 +0000

    Section 14 -> 120. Updating our existing tests to handle sessions

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"testing"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -23,6 +24,45 @@ var app config.AppConfig
 var session *scs.SessionManager
 var pathToTemplates = "../../templates"
 var functions = template.FuncMap{}
+
+func TestMain(m *testing.M) {
+	// START main.go::run()
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// Change this to true when in production
+	app.InProduction = false
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := NewTestRepo(&app)
+	NewHandlers(repo)
+
+	render.NewRenderer(&app)
+	// END main.go::run()
+
+	os.Exit(m.Run())
+}
 
 func getRoutes() http.Handler {
 	// START main.go::run()
